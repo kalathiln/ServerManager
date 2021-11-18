8ae43e2a953ff04ac1faab15fb555ebaf2a79f85
kalathiln <nikhkk@gmail.com>
2021-11-18 12:31:32 +0100

    ADDED CHANGES TO THE SERVER CONFIG

--- src/main/java/com/blackmolecaptures/server/ServerApplication.java
+++ src/main/java/com/blackmolecaptures/server/ServerApplication.java
@@ -10,22 +10,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
-	// Bean commandline runner bean
+    // Bean commandline runner bean
 
-	CommandLineRunner run(ServerRepo serverRepo){
-		return args -> {
-			serverRepo.save(new Server(null, "192.168.1.160","UBUNTU LINUX", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png",
-					Status.SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.161","MACOS", "16 GB", "Personal PC", "http://localhost:8080/server/image/server2.png",
-					Status.SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.162","Windows", "16 GB", "Personal PC", "http://localhost:8080/server/image/server3.png",
-					Status.SERVER_UP));
-			serverRepo.save(new Server(null, "192.168.1.163","NAS", "16 GB", "Personal PC", "http://localhost:8080/server/image/server4.png",
-					Status.SERVER_DOWN));
-		};
-	};
+    CommandLineRunner run(ServerRepo serverRepo) {
+        return args -> {
+            serverRepo.save(new Server(null, "192.168.1.160", "UBUNTU LINUX", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png",
+                    Status.SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.161", "MACOS", "16 GB", "Personal PC", "http://localhost:8080/server/image/server2.png",
+                    Status.SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.162", "Windows", "16 GB", "Personal PC", "http://localhost:8080/server/image/server3.png",
+                    Status.SERVER_UP));
+            serverRepo.save(new Server(null, "192.168.1.163", "NAS", "16 GB", "Personal PC", "http://localhost:8080/server/image/server4.png",
+                    Status.SERVER_DOWN));
+        };
+    };
 }
